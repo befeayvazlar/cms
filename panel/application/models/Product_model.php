3a3317f99c14a0469791931d8c68f52ad25ff8b5
deal creative <webdealcreative@gmail.com>
2020-04-04 12:59:48 +0300

    Model içersinde get() ve get_all() metodları dinamikleştirildi. Ürün update view tasarımı gerçekleştirildi.

--- panel/application/models/Product_model.php
+++ panel/application/models/Product_model.php
@@ -8,9 +8,14 @@ class Product_model extends CI_Model{
 
 	}
 
+	public function get($where = array()){
+		return $this->db->where($where)->get($this->tableName)->row();
+
+	}
+
 	/** Tüm kayıtları bana getirecek olan metot. */
-	public function get_all() {
-		return $this->db->get($this->tableName)->result();
+	public function get_all($where = array()) {
+		return $this->db->where($where)->get($this->tableName)->result();
 
 	}
 	public function add($data = array()) {
