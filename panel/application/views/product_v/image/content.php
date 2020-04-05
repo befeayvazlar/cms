03d7b64eb95c7105d26af3d9f088def494c6d7c2
deal creative <webdealcreative@gmail.com>
2020-04-05 04:24:58 +0300

    Upload işlemi yapıldı.

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -1,16 +1,11 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">
-            Ürün Resimleri
-        </h4>
-    </div><!-- END column -->
-    <div class="col-md-12">
         <div class="widget">
             <div class="widget-body">
-                <form action="../api/dropzone" class="dropzone" data-plugin="dropzone" data-options="{ url: '../api/dropzone'}">
+                <form action="<?php echo base_url("product/image_upload"); ?>" class="dropzone" data-plugin="dropzone" data-options="{ url: '<?php echo base_url("product/image_upload"); ?>'}">
                     <div class="dz-message">
-                        <h3 class="m-h-lg">Drop files here or click to upload.</h3>
-                        <p class="m-b-lg text-muted">(This is just a demo dropzone. Selected files are not actually uploaded.)</p>
+                        <h3 class="m-h-lg">Yüklemek istediğiniz resimleri buraya sürükleyiniz.</h3>
+                        <p class="m-b-lg text-muted">(Yüklemek için dosyalarınız sürükleyiniz ya da buraya tıklayız.)</p>
                     </div>
                 </form>
             </div><!-- .widget-body -->
@@ -21,7 +16,7 @@
 <div class="row">
     <div class="col-md-12">
         <h4 class="m-b-lg">
-            Ürün Resimleri
+            <?php echo "<b>$item->title</b>"; ?> kaydına ait Resimler
         </h4>
     </div><!-- END column -->
     <div class="col-md-12">
